23d7ab0610ca43e907444e48adf1a4002f651a64
Dan <danielshorsey05@yahoo.co.uk>
2020-07-24 11:25:08 +0100

    Something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel DANS TEST
                 </div>
 
                 <div class="links">
